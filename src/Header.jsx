2d568640b07c94ac2c5521c0aa320fcfe2e23139
Maalini <maalini@innoventes.co>
2019-02-12 18:27:52 +0530

    First Commit

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div id ="navbar">
+                <span>Vazhga vaiyagham! Vazhga valamudan!!</span>
+                <span>PROGRAMS</span>
+                <span>ARIVAE THEIVAM</span>
+                <span>DONATION</span>
+                <span>TEAM</span>
+                <span>CONTACT US</span>
+            </div>
+
+         );
+    }
+}
+ 
+export default Header;
